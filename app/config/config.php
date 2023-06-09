474f619bc49f4af36b1934b1eae40258bbe9a9c1
Rizky Fajar Maulana <78131780+LenixRiz@users.noreply.github.com>
2023-06-09 08:52:21 +0700

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL','http://localhost/web-praktisi/public');
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'web_praktisi');
